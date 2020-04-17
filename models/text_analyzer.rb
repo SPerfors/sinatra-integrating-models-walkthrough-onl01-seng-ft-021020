2ec650c9f87a4e14924dabb4c9e799cf6414a560
Steven Perfors <stevecanbuild@gmail.com>
2020-04-17 20:34:41 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,42 +1,42 @@
-class TextAnalyzer 
-  attr_reader :text 
-  
+class TextAnalyzer
+  attr_reader :text
+ 
   def initialize(text)
     @text = text.downcase
   end
-  
+ 
   def count_of_words
     words = text.split(" ")
-    words.count 
+    words.count
   end
-  
-  def count_of_vowels 
-    text.scan(/[aeoui]/).count 
+ 
+  def count_of_vowels
+    text.scan(/[aeoui]/).count
   end
-  
-  def count_of_consonants 
+ 
+  def count_of_consonants
     text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
-  
-  def most_used_letter 
-    sl = text.gsub(/^[a-z]/, '') # gets rid of spaces 
-    arr = sl.split('')
-    arr1 = arr.uniq 
+ 
+  def most_used_letter
+    s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
+    arr = s1.split('')
+    arr1 = arr.uniq
     arr2 = {}
-    
+ 
     arr1.map do |c|
-      arr2[c] = arr.count(c) 
+      arr2[c] =  arr.count(c)
     end
-    
-    biggest = { arr2.keys.first +> arr2.values.first }
-    
+ 
+    biggest = { arr2.keys.first => arr2.values.first }
+ 
     arr2.each do |key, value|
-      if value > biggest.values.first 
+      if value > biggest.values.first
         biggest = {}
-        biggest[key] = value 
+        biggest[key] = value
       end
     end
-    
-    biggest 
+ 
+    biggest
   end
 end
